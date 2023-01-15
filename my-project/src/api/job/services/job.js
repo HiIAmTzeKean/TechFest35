43dad641647a8da374f624e29dcfcb358249daab
indicium15 <67154087+indicium15@users.noreply.github.com>
2023-01-15 16:16:18 +0800

    added job role and test job

--- my-project/src/api/job/services/job.js
+++ my-project/src/api/job/services/job.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * company service
+ * job service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::company.company');
+module.exports = createCoreService('api::job.job');
